4d2caa2a60607095f7bc12de750769217c473fb4
dakkuti <dianarojas3245@gmail.com>
2021-09-11 23:49:44 -0500

    TERMINANDO MAQUETACION

---
+++ src/components/Historial.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { verificar } from '../helpers/Verificar'
+
+export const Historial = () => {
+    return (
+        <div>
+            <verificar/>
+            <h1>Historial</h1>
+        </div>
+    )
+}
